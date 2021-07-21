04a9554e61983c9c6274fda18c04af3f02dca6f1
Frago9876543210 <al.volf.av@gmail.com>
2021-07-21 21:51:53 +0300

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,4 +1,5 @@
 $ProgressPreference = "SilentlyContinue"
 
+$OUTPUT = "bedrock_server.zip"
 Invoke-WebRequest -Uri "https://minecraft.azureedge.net/bin-linux/bedrock-server-1.12.1.1.zip" -OutFile $OUTPUT
 7z x $OUTPUT
