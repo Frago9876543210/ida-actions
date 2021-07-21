7da4018d7e206c4e59a4151585a25c527d9c405e
Frago9876543210 <al.volf.av@gmail.com>
2021-07-21 21:45:28 +0300

    BDS 1.12.1.1

--- download.ps1
+++ download.ps1
@@ -1,12 +1,4 @@
 $ProgressPreference = "SilentlyContinue"
 
-$URL = "https://minecraft.net/en-us/download/server/bedrock/"
-$DOWNLOAD = (Invoke-WebRequest $URL -Headers @{"Accept"="text/html"}).Content | Select-String -Pattern "https:\/\/\S*\/bin-linux\/\S*" | Foreach-Object {$_.Matches.Value}
-$DOWNLOAD = $DOWNLOAD.Substring(0, $DOWNLOAD.Length - 1)
-$VERSION = $DOWNLOAD | Select-String -Pattern "(\d+\.){3}\d+" | Foreach-Object {$_.Matches.Value}
-$OUTPUT = "bedrock_server.zip"
-
-echo "Found bedrock_server $VERSION"
-
-Invoke-WebRequest -Uri $DOWNLOAD -OutFile $OUTPUT
+Invoke-WebRequest -Uri "https://minecraft.azureedge.net/bin-linux/bedrock-server-1.12.1.1.zip" -OutFile $OUTPUT
 7z x $OUTPUT
